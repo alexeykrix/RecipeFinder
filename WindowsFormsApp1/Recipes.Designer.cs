507cc8d68daa6ea2eaaaa304b1ac07300da863d0
Alexey Krix <alexey@thefirstthelast.agency>
2023-04-23 20:07:04 +0300

    Add project files.

---
+++ WindowsFormsApp1/Recipes.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Recipes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.flowLayout = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // inputSearch
+            // 
+            this.inputSearch.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.inputSearch.Location = new System.Drawing.Point(242, 12);
+            this.inputSearch.Name = "inputSearch";
+            this.inputSearch.Size = new System.Drawing.Size(532, 26);
+            this.inputSearch.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnSearch.BackColor = System.Drawing.Color.Transparent;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
+            this.btnSearch.Location = new System.Drawing.Point(799, 9);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(93, 32);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // flowLayout
+            // 
+            this.flowLayout.AutoScroll = true;
+            this.flowLayout.BackColor = System.Drawing.Color.Coral;
+            this.flowLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayout.Location = new System.Drawing.Point(0, 0);
+            this.flowLayout.Name = "flowLayout";
+            this.flowLayout.Padding = new System.Windows.Forms.Padding(0, 50, 0, 50);
+            this.flowLayout.Size = new System.Drawing.Size(1224, 688);
+            this.flowLayout.TabIndex = 2;
+            // 
+            // Recipes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1224, 688);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.inputSearch);
+            this.Controls.Add(this.flowLayout);
+            this.Name = "Recipes";
+            this.Text = "Recipes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.FlowLayoutPanel flowLayout;
+    }
+}
---
+++ WindowsFormsApp1/Recipes.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static WindowsFormsApp1.Recipes;
+
+namespace WindowsFormsApp1
+{
+    public partial class Recipes : Form
+    {
+        public Recipes()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+            string userInput = inputSearch.Text.Replace(" ", ",+");
+            string apiKey = "&apiKey=22d37f4ddb3649d1aa1ee372e930b27c";
+
+            string apiUrl = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + userInput + apiKey;
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    List<Recipe> recipeList = await response.Content.ReadAsAsync<List<Recipe>>();
+
+                    DisplayRecipes(recipeList);
+                }
+                else
+                {
+                    MessageBox.Show("Error: Could not retrieve search results");
+                }
+            }
+        }
+
+        private void DisplayRecipes(List<Recipe> recipeList)
+        {
+            flowLayout.Controls.Clear();
+
+            foreach (Recipe recipe in recipeList)
+            {
+                RecipeCard card = new RecipeCard(recipe);
+                flowLayout.Controls.Add(card);
+            }
+        }
+
+        public class Ingredient
+        {
+            public string name { get; set; }
+            public string original { get; set; }
+        }
+
+        public class Recipe
+        {
+            public string title { get; set; }
+            public string image { get; set; }
+            public string Description { get; set; }
+            public List<Ingredient> missedIngredients { get; set; }
+            public List<Ingredient> usedIngredients { get; set; }
+        }
+
+        public class RecipeCard : UserControl
+        {
+            private PictureBox pictureBox;
+            private Label nameLabel;
+            private FlowLayoutPanel ingredientsPanel;
+
+            public RecipeCard(Recipe recipe)
+            {
+                pictureBox = new PictureBox();
+                pictureBox.Size = new Size(200, 200);
+                pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox.LoadAsync(recipe.image);
+
+                nameLabel = new Label();
+                nameLabel.Text = recipe.title;
+                nameLabel.Font = new Font("Segoe UI", 12, FontStyle.Bold);
+                nameLabel.AutoSize = true;
+
+                ingredientsPanel = new FlowLayoutPanel();
+                ingredientsPanel.FlowDirection = FlowDirection.TopDown;
+                ingredientsPanel.AutoSize = true;
+                ingredientsPanel.Margin = new Padding(200, 10, 0, 0);
+                ingredientsPanel.Location = new Point(200, 0);
+
+                ingredientsPanel.Controls.Add(nameLabel);
+
+                foreach (Ingredient ingredient in recipe.missedIngredients)
+                {
+                    Label ingredientLabel = new Label();
+                    ingredientLabel.Text = ingredient.name;// + " (" + ingredient.original + ")";
+                    ingredientLabel.ForeColor = Color.Red;
+                    ingredientLabel.AutoSize = true;
+                    ingredientsPanel.Controls.Add(ingredientLabel);
+                }
+
+                foreach (Ingredient ingredient in recipe.usedIngredients)
+                {
+                    Label ingredientLabel = new Label();
+                    ingredientLabel.Text = ingredient.name;// + " (" + ingredient.original + ")";
+                    ingredientLabel.ForeColor = Color.Green;
+                    ingredientLabel.AutoSize = true;
+                    ingredientsPanel.Controls.Add(ingredientLabel);
+                }
+
+                this.Controls.Add(pictureBox);
+                this.Controls.Add(ingredientsPanel);
+                this.AutoSize = true;
+                this.Margin = new Padding(10);
+                this.BorderStyle = BorderStyle.FixedSingle;
+            }
+        }
+
+    }
+}
